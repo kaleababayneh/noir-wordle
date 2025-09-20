870bf2db2b54832b7c2cdd37521dd1f2e58cb919
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2025-09-20 16:19:18 +0300

    start frontend integration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all your React files
+    "./frontend/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
